e0376fd34e019478045312326c641fb5a207b286
gio <95066222+gioAlgorithm@users.noreply.github.com>
2023-04-06 00:43:01 +0400

    push

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import jwt_decode from "jwt-decode"
 import { LoginSocialFacebook } from "reactjs-social-login"
-import { FacebookLoginButton } from "react-social-login-buttons"
+import { FaFacebookF } from "react-icons/fa";
 import logo from '../image/logo.png'
 import "./css/navbar.css"
 
@@ -12,12 +12,12 @@ export default function Navbar(){
     function handleCallbackResponse(response){
         var userObject = jwt_decode(response.credential)
         setUser(userObject)
-        document.getElementById("signInDiv").hidden = true
+        
     }
 
     function handleSignOut(event){
         setUser({})
-        document.getElementById("signInDiv").hidden = false
+       
     }
 
     useEffect(()=>{
@@ -27,25 +27,37 @@ export default function Navbar(){
             callback: handleCallbackResponse
         })
 
-        google.accounts.id.renderButton(
-            document.getElementById("signInDiv"),
-            {theme: "outline", size:"medium"}
-        )
-    }, [])
+        if (Object.keys(user).length === 0) {
+            google.accounts.id.renderButton(
+              document.getElementById("signInDiv"),
+              {
+                theme: "outline",
+                size: "medium",
+              }
+            )
+          }
+    }, [user])
 
 
     return(
         <div className="navbar">
-            <img alt="logo" src={logo}/>
-            <div id="signInDiv"></div>
-            <LoginSocialFacebook 
-                appId="895324061700196"
-                onResolve={(response) =>{
-                    setUser(response.data)
-                }}
-            >
-                <FacebookLoginButton />
-            </LoginSocialFacebook>
+            <img alt="logo" src={logo} className="logo"/>
+            {Object.keys(user).length === 0 &&
+                <div className="sign-in">
+                    <div id="signInDiv"></div>
+                    <LoginSocialFacebook 
+                        appId="895324061700196"
+                        
+                        onResolve={(response) =>{
+                            setUser(response.data)
+                        }}
+                    >
+                        <button className="facebook-button">
+                            <FaFacebookF /><span>Sign in with Facebook</span> 
+                        </button>
+                    </LoginSocialFacebook>
+                </div>
+            }
             { Object.keys(user).length !== 0 &&
                 <button onClick={(e)=> handleSignOut(e)}>sign out</button>
 
