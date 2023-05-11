a1ff020aca003ac79eed44e36e87293bf8d05778
gio <95066222+gioAlgorithm@users.noreply.github.com>
2023-05-11 04:54:41 +0400

    push

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react"
 import { Route, Routes } from "react-router-dom"
 import Main from "./components/Main"
 import SharedLayout from "./SharedLayout"
-
 import "./App.css"
 import ProfilePage from "./components/ProfilePage"
 
 
 
 export default function App(){
-
   return(
     <Routes>
       <Route path="/" element={<SharedLayout />}>
